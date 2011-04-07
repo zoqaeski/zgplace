8e75c3086adec9b5098e2151a03bee459b3a4183
Robbie <zoqaeski@gmail.com>
2011-04-07 17:28:07 +1000

    More work on caching files. Not sure how to implement a more thorough degree of protection against file changes.

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once(dirname(__FILE__) . '/../include/php/application.php');
 
 try {
 	$zgplace = new Application($_GET);
+	print $zgplace->run();
 }
 catch (Exception $e) {
 	echo $e->getMessage(), "\n";
